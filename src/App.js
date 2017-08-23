340764f95b2eb94d0b3d342fe4b99b86acdb83ff
MKuzmichev <MKuzmichev@competentum.ru>
2017-08-23 16:26:10 +0400

    simple router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 import Footer from './footer/Footer';
 import Header from './header/Header';
 import MainBlock from './main-block/MainBlock';
+import Admin from './Admin/Admin';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      route: window.location.hash.substr(1)
+    }
+  }
+
+  componentDidMount() {
+    console.log(window.location.hash);
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      });
+    })
+  }
+
   render() {
+    let Child;
+
+    switch (this.state.route) {
+      case '/admin' :
+        Child = Admin;
+        break;
+      case '/main' :
+        Child = MainBlock;
+        break;
+      default :
+        Child = MainBlock;
+    }
+
     return (
       <div className="App">
         <Header />
-        <MainBlock />
+        <Child />
         <Footer />
       </div>
     );
